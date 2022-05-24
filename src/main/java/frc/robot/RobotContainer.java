58bb53e8ef3d4f2ccbcc561b69c62db756ab51f7
Josiah Fu <josiah_fu@student.davincischools.org>
2022-05-23 22:36:39 -0700

    Fixed time control system

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,6 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import java.util.TreeMap;
@@ -30,7 +29,6 @@ public class RobotContainer {
 
     private final Field2d field2d = new Field2d();
     private double currentTime = 0;
-    private double endTime;
     private double lastLoopTime = 0;
     private boolean playing = true;
 
@@ -42,11 +40,12 @@ public class RobotContainer {
     public RobotContainer() {
         SmartDashboard.putData("Field2d", field2d);
         SmartDashboard.putNumber("Time", 0);
-        SmartDashboard.putData("\u25b6", new InstantCommand().andThen(this::play));
-        SmartDashboard.putData("\u2358", new InstantCommand().andThen(this::pause));
+        SmartDashboard.putBoolean("Play", playing);
+
         // Configure the button bindings
         configureButtonBindings();
         int poseEntryId = 0;
+
         try {
             DataLogReader reader = new DataLogReader("src/main/deploy/FRC_20220523_024716.wpilog");
             String[] poseParts;
@@ -57,10 +56,6 @@ public class RobotContainer {
                 if (record.isStart()) {
                     if (record.getStartData().name.equals("/pose")) {
                         poseEntryId = record.getStartData().entry;
-                    }
-                } else if (record.isFinish()) {
-                    if (record.getFinishEntry() == poseEntryId) {
-                        endTime = record.getTimestamp() / 1000000.0;
                     }
                 } else if (record.getEntry() == poseEntryId) {
                     poseParts = record.getString().split(",");
@@ -74,7 +69,7 @@ public class RobotContainer {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        SmartDashboard.putNumber("Max", endTime);
+        SmartDashboard.putNumber("Max", robotPoses.lastKey());
     }
 
     /**
@@ -97,28 +92,20 @@ public class RobotContainer {
     }
 
     public void simulationPeriodic() {
-        currentTime = SmartDashboard.getNumber("Time", currentTime);
-        try {
-            field2d.setRobotPose(
-                    robotPoses.floorEntry(
-                            Timer.getFPGATimestamp()
-                    ).getValue());
-            System.out.println(robotPoses.floorEntry(currentTime).getValue());
-        } catch (NullPointerException ignored) {
+        if (!playing && SmartDashboard.getBoolean("Play", false)) {
+            lastLoopTime = Timer.getFPGATimestamp();
         }
-        if (playing) {
+        playing = SmartDashboard.getBoolean("Play", false);
+        currentTime = SmartDashboard.getNumber("Time", currentTime);
+        if (currentTime < robotPoses.firstKey()) {
+            field2d.setRobotPose(robotPoses.firstEntry().getValue());
+        } else {
+            field2d.setRobotPose(robotPoses.floorEntry(currentTime).getValue());
+        }
+        if (playing && currentTime < robotPoses.lastKey()) {
             currentTime += Timer.getFPGATimestamp() - lastLoopTime;
             SmartDashboard.putNumber("Time", currentTime);
             lastLoopTime = Timer.getFPGATimestamp();
         }
-    }
-
-    public void play() {
-        lastLoopTime = Timer.getFPGATimestamp();
-        playing = true;
-    }
-
-    public void pause() {
-        playing = false;
     }
 }
