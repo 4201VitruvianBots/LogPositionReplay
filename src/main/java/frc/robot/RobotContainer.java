43ec54b394f4b854ef3b1033836a443538c4151a
Josiah Fu <josiah_fu@student.davincischools.org>
2022-05-23 21:58:47 -0700

    Format Code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,8 +7,6 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.util.datalog.DataLogEntry;
-import edu.wpi.first.util.datalog.DataLogIterator;
 import edu.wpi.first.util.datalog.DataLogReader;
 import edu.wpi.first.util.datalog.DataLogRecord;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -17,18 +15,9 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Optional;
 import java.util.TreeMap;
-
-import com.opencsv.CSVReader;
-import org.apache.commons.lang3.ObjectUtils;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -37,98 +26,99 @@ import org.apache.commons.lang3.ObjectUtils;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    // The robot's subsystems and commands are defined here...
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+    private final Field2d field2d = new Field2d();
+    private double currentTime = 0;
+    private double endTime;
+    private double lastLoopTime = 0;
+    private boolean playing = true;
 
-  private final Field2d field2d = new Field2d();
-  private double currentTime = 0;
-  private double endTime;
-  private double lastLoopTime = 0;
-  private boolean playing = true;
+    private final TreeMap<Double, Pose2d> robotPoses = new TreeMap<>();
 
-  private final TreeMap<Double,Pose2d> robotPoses = new TreeMap<>();
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+        SmartDashboard.putData("Field2d", field2d);
+        SmartDashboard.putNumber("Time", 0);
+        SmartDashboard.putData("\u25b6", new InstantCommand().andThen(this::play));
+        SmartDashboard.putData("\u2358", new InstantCommand().andThen(this::pause));
+        // Configure the button bindings
+        configureButtonBindings();
+        int poseEntryId = 0;
+        try {
+            DataLogReader reader = new DataLogReader("src/main/deploy/FRC_20220523_024716.wpilog");
+            String[] poseParts;
+            double time;
+            double x, y, angle;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    SmartDashboard.putData("Field2d",field2d);
-    SmartDashboard.putNumber("Time", 0);
-    SmartDashboard.putData("\u25b6", new InstantCommand().andThen(this::play));
-    SmartDashboard.putData("\u2358", new InstantCommand().andThen(this::pause));
-    // Configure the button bindings
-    configureButtonBindings();
-    int poseEntryId = 0;
-      try {
-        DataLogReader reader = new DataLogReader("src/main/deploy/FRC_20220523_024716.wpilog");
-        String[] poseParts;
-        double time;
-        double x, y, angle;
-
-        for (DataLogRecord record : reader) {
-          if (record.isStart()) {
-            if (record.getStartData().name.equals("/pose")) {
-              poseEntryId = record.getStartData().entry;
+            for (DataLogRecord record : reader) {
+                if (record.isStart()) {
+                    if (record.getStartData().name.equals("/pose")) {
+                        poseEntryId = record.getStartData().entry;
+                    }
+                } else if (record.isFinish()) {
+                    if (record.getFinishEntry() == poseEntryId) {
+                        endTime = record.getTimestamp() / 1000000.0;
+                    }
+                } else if (record.getEntry() == poseEntryId) {
+                    poseParts = record.getString().split(",");
+                    x = Double.parseDouble(poseParts[0]);
+                    y = Double.parseDouble(poseParts[1]);
+                    angle = Double.parseDouble(poseParts[2]);
+                    time = record.getTimestamp() / 1000000.0;
+                    robotPoses.put(time, new Pose2d(new Translation2d(x, y), new Rotation2d(angle)));
+                }
             }
-          } else if (record.isFinish()) {
-            if (record.getFinishEntry() == poseEntryId) {
-              endTime = record.getTimestamp() / 1000000.0;
-            }
-          } else if (record.getEntry() == poseEntryId) {
-            poseParts = record.getString().split(",");
-            x = Double.parseDouble(poseParts[0]);
-            y = Double.parseDouble(poseParts[1]);
-            angle = Double.parseDouble(poseParts[2]);
-            time = record.getTimestamp() / 1000000.0;
-            robotPoses.put(time, new Pose2d(new Translation2d(x, y), new Rotation2d(angle)));
-          }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
         SmartDashboard.putNumber("Max", endTime);
-  }
-
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
-
-  public void simulationPeriodic() {
-    currentTime = SmartDashboard.getNumber("Time", currentTime);
-    try {
-      field2d.setRobotPose(
-              robotPoses.floorEntry(
-                      Timer.getFPGATimestamp()
-              ).getValue());
-      System.out.println(robotPoses.floorEntry(currentTime).getValue());
-    } catch(NullPointerException ignored) {}
-    if (playing) {
-      currentTime += Timer.getFPGATimestamp() - lastLoopTime;
-      SmartDashboard.putNumber("Time", currentTime);
-      lastLoopTime = Timer.getFPGATimestamp();
     }
-  }
 
-  public void play() {
-    lastLoopTime = Timer.getFPGATimestamp();
-    playing = true;
-  }
+    /**
+     * Use this method to define your button->command mappings. Buttons can be created by
+     * instantiating a {@link GenericHID} or one of its subclasses ({@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+    }
 
-  public void pause() {
-    playing = false;
-  }
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An ExampleCommand will run in autonomous
+        return null;
+    }
+
+    public void simulationPeriodic() {
+        currentTime = SmartDashboard.getNumber("Time", currentTime);
+        try {
+            field2d.setRobotPose(
+                    robotPoses.floorEntry(
+                            Timer.getFPGATimestamp()
+                    ).getValue());
+            System.out.println(robotPoses.floorEntry(currentTime).getValue());
+        } catch (NullPointerException ignored) {
+        }
+        if (playing) {
+            currentTime += Timer.getFPGATimestamp() - lastLoopTime;
+            SmartDashboard.putNumber("Time", currentTime);
+            lastLoopTime = Timer.getFPGATimestamp();
+        }
+    }
+
+    public void play() {
+        lastLoopTime = Timer.getFPGATimestamp();
+        playing = true;
+    }
+
+    public void pause() {
+        playing = false;
+    }
 }
